d23f391378ff1749fe8cae51a819e0326fea78f1
harsheeyy04 <kanwarlaxmi0100@gmail.com>
2026-08-10 21:56:39 +0530

    day 1 , question 1

---
+++ day01.c
@@ -0,0 +1,12 @@
+ques 1 . write a program to input two numbers and display their sum 
+#include<stdio.h>
+int main() {
+  int a,b,sum;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+sum=a+b;
+printf("sum = %d\n", sum);
+return 0;
+}
