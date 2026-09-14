64e2b06331f6326e925a02868ff7941eba2ee784
harsheeyy04 <kanwarlaxmi0100@gmail.com>
2026-09-14 12:12:04 +0530

    Create day34q1.c

---
+++ day34q1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, pos, value;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    printf("Enter value to insert: ");
+    scanf("%d", &value);
+
+    for(i = n; i >= pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = value;
+    n++;
+
+    printf("Array after insertion: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
