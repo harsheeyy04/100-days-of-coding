83b85394bbc1fbf3b7d61de776481c8aa07a8ade
harsheeyy04 <kanwarlaxmi0100@gmail.com>
2026-09-14 10:53:57 +0530

    day20q2.c

---
+++ day20q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n, digit, place = 1, result = 0;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+
+    while(n > 0)
+    {
+        digit = n % 10;
+
+        if(digit == 0)
+            digit = 1;
+        else
+            digit = 0;
+
+        result = result + digit * place;
+        place = place * 10;
+        n = n / 10;
+    }
+
+    printf("1's complement = %lld", result);
+
+    return 0;
+}
